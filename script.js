41ab7366b240f7c76c11896667f0e28fb5c3d666
Jurriaan <jurriaan@mbpvanjurriaan.guest.tno.nl>
2019-05-08 11:01:21 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+function sendEmail() 
+        {window.location ="mailto:jfparie@me.com";};
+
 $(document).ready(function() {
     $("#pi-hex").click(function() {
-        $(".row2").slideToggle(300);
+        $(".row-pi").slideToggle(300);
     });
 });
 
-function sendEmail() 
-        {window.location ="mailto:jfparie@me.com";}
+$(document).ready(function() {
+    $("#data-hex").click(function() {
+        $(".row-data").slideToggle(300);
+    });
+});
+
+$(document).ready(function() {
+    $("#NDT-hex").click(function() {
+        $(".row-NDT").slideToggle(300);
+    });
+});
+
+$(document).ready(function() {
+    $("#hobby-hex").click(function() {
+        $(".row-hobby").slideToggle(300);
+    });
+});
+
+
