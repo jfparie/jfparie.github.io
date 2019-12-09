72efa04ddd4f0e7f3befb96aeaea47240e3fd75c
Jurriaan <jurriaan@MacBook-Pro-van-Jurriaan.local>
2019-12-09 11:32:07 +0100

    added scroll anchors

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function sendEmail()
 $(document).ready(function() {
     $("#pi-hex").click(function() {
         $("#row-pi").slideToggle(300);
+        $('html,body').animate({
+        scrollTop: $("#row-pi").offset().top},
+        'slow');
     });
 });
 
 $(document).ready(function() {
     $("#data-hex").click(function() {
         $("#row-data").slideToggle(300);
+        $('html,body').animate({
+        scrollTop: $("#row-data").offset().top},
+        'slow');
     });
 });
 
 $(document).ready(function() {
     $("#NDT-hex").click(function() {
         $("#row-NDT").slideToggle(300);
+        $('html,body').animate({
+        scrollTop: $("#row-NDT").offset().top},
+        'slow');
     });
 });
 
 $(document).ready(function() {
     $("#hobby-hex").click(function() {
         $("#row-hobby").slideToggle(300);
+        $('html,body').animate({
+        scrollTop: $("#row-hobby").offset().top},
+        'slow');
     });
 });
-
-
