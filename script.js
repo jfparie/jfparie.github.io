99d26642450e4b54a283744b5e233bae0a78d586
jfparie <jfparie@me.com>
2021-09-14 22:14:32 +0200

    update

--- script.js
+++ script.js
@@ -1,59 +1,84 @@
-function sendEmail() 
-        {window.location ="mailto:jfparie@me.com";};
-
-$(document).ready(function() {
-    $("#logo").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#lightblue1-area").offset().top},
-        'slow');
-    });
-});
-
-$(document).ready(function() {
-    $("#eth-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#blue-area").offset().top},
-        'slow');
-    });
-});
-
-$(document).ready(function() {
-    $("#news-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#green-area").offset().top},
-        'slow');
-    });
-});
-
-
-$(document).ready(function() {
-    $("#pi-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#yellow-area").offset().top},
-        'slow');
-    });
-});
-
-$(document).ready(function() {
-    $("#lse-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#red-area").offset().top},
-        'slow');
-    });
-});
-
-$(document).ready(function() {
-    $("#NDT-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#violet-area").offset().top},
-        'slow');
-    });
-});
-
-$(document).ready(function() {
-    $("#hobby-hex").click(function() {
-        $('html,body').animate({
-        scrollTop: $("#lightblue2-area").offset().top},
-        'slow');
-    });
-});
+function check_width() { 
+    if (window.innerWidth < 960) {
+      window.location = "index_mobile.html";
+      }
+    if (window.innerWidth >= 960) {
+        window.location = "index.html";
+    }
+  }
+  
+  function sendEmail() 
+          {window.location ="mailto:jfparie@me.com";};
+  
+  $(document).ready(function() {
+      $("#logo").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#JFP_bio").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#eth-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#eth-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#news-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#news-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  
+  $(document).ready(function() {
+      $("#pi-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#pi-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#lse-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#lse-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#NDT-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#ndt-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#hobby-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#hobby-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#ibm-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#ibm-icon").offset().top},
+          'slow');
+      });
+  });
+  
+  $(document).ready(function() {
+      $("#aa-hex").click(function() {
+          $('html,body').animate({
+          scrollTop: $("#aa-icon").offset().top},
+          'slow');
+      });
+  });
